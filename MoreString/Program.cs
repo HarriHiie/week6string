b3fb9d0519616af01e89de8d99844b2d125de338
Opilane TTHK <opilane@tthk.ee>
2021-10-06 11:32:40 +0300

    strings

---
+++ MoreString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MoreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("sisesta oma eesnimi");
+            string userfirstname = Console.ReadLine();
+
+            Console.WriteLine("Sisesta oma perekonnanimi");
+            string userlastname = Console.ReadLine();
+
+            Console.WriteLine($"Tere, {userfirstname[0]}. {userlastname[0]}.!");
+
+
+
+
+
+        }
+    }
+}
